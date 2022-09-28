d4852214841adfbad6e917e90722bee935f7d376
jovaniq1 <jovaniq1@gmail.com>
2022-09-27 22:00:37 -0500

    Added env variables

--- util/database.js
+++ util/database.js
@@ -1,5 +1,10 @@
 const Sequelize = require('sequelize').Sequelize;
 
+// const sequelize = new Sequelize('workoutStats', 'root', '123456', {
+//   dialect: 'mysql',
+//   host: '35.224.159.246',
+// });
+
 const sequelize = new Sequelize(
   'workoutStats',
   'root',
