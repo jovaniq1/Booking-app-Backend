362590654040a218cc3e9e28cb6fa84755a25dc1
jovaniq1 <jovaniq1@gmail.com>
2022-09-28 10:02:03 -0500

    Added env variables 2

--- util/database.js
+++ util/database.js
@@ -1,11 +1,26 @@
 const Sequelize = require('sequelize').Sequelize;
 
 const sequelize = new Sequelize('workoutStats', 'root', '123456', {
-  dialect: 'mysql',
   host: '35.224.159.246',
+  dialect: 'mysql',
   port: 3306,
+  define: {
+    charset: 'utf8mb4',
+    // collate: 'utf8mb4_unicode_ci'
+  },
+  dialectOptions: {
+    charset: 'utf8mb4',
+  },
+  logging: false,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
 
+console.log('---process.env', process.env);
 sequelize
   .authenticate()
   .then(() => {
